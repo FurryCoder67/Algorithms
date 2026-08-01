fcb5e6bb21871ca72ec58dd2e2eaeffbe601ae94
Aiden <aiden@rivi.us>
2026-08-01 16:03:32 -0400

    a

---
+++ magic_square/target/debug/deps/magic_square.d
@@ -0,0 +1,5 @@
+C:\Users\aiden\Documents\Code\Algorithms\magic_square\target\debug\deps\magic_square.d: src\main.rs
+
+C:\Users\aiden\Documents\Code\Algorithms\magic_square\target\debug\deps\magic_square.exe: src\main.rs
+
+src\main.rs:
---
+++ magic_square/target/debug/magic_square.d
@@ -0,0 +1,1 @@
+C:\Users\aiden\Documents\Code\Algorithms\magic_square\target\debug\magic_square.exe: C:\Users\aiden\Documents\Code\Algorithms\magic_square\src\main.rs
