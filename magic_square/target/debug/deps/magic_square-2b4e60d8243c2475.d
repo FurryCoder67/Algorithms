57a4e49d968b050a73e2bc9cb697c1e99849b0c0
Aiden <aiden@rivi.us>
2026-08-01 16:07:41 -0400

    a

---
+++ magic_square/target/debug/deps/magic_square-2b4e60d8243c2475.d
@@ -0,0 +1,5 @@
+C:\Users\aiden\Documents\Code\Algorithms\magic_square\target\debug\deps\magic_square-2b4e60d8243c2475.d: src\main.rs
+
+C:\Users\aiden\Documents\Code\Algorithms\magic_square\target\debug\deps\libmagic_square-2b4e60d8243c2475.rmeta: src\main.rs
+
+src\main.rs:
---
+++ magic_square/target/debug/deps/magic_square-b6340237b2d0bf68.d
@@ -0,0 +1,5 @@
+C:\Users\aiden\Documents\Code\Algorithms\magic_square\target\debug\deps\magic_square-b6340237b2d0bf68.d: src\main.rs
+
+C:\Users\aiden\Documents\Code\Algorithms\magic_square\target\debug\deps\libmagic_square-b6340237b2d0bf68.rmeta: src\main.rs
+
+src\main.rs:
