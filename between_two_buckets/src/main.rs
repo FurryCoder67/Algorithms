a999e3941a7fd8c2921552f7ba7dbc0f13ff5816
Aiden <aiden@rivi.us>
2026-08-09 13:54:04 -0400

    a

--- between_two_buckets/src/main.rs
+++ between_two_buckets/src/main.rs
@@ -9,9 +9,7 @@ fn mix_paint(a: Bucket, b: Bucket) -> [i32; 3] {
 
     [
         ((a.color[0] * a.fullness + b.color[0] * b.fullness) as f64 / total_fullness as f64).round() as i32,
-
         ((a.color[1] * a.fullness + b.color[1] * b.fullness) as f64 / total_fullness as f64).round() as i32,
-
         ((a.color[2] * a.fullness + b.color[2] * b.fullness) as f64 / total_fullness as f64).round() as i32,
     ]
 }
