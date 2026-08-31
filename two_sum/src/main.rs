5bbe1dec6377b64e4ea44a8af49dbe1af6464c69
Aiden <aiden@rivi.us>
2026-08-31 11:02:28 -0400

    a

--- two_sum/src/main.rs
+++ two_sum/src/main.rs
@@ -1,14 +1,13 @@
 // given an array of integers and a target value, return the indices of the two numbers that add up to the target
 fn two_sum(nums: Vec<i32>, target: i32) -> (usize, usize) {
-    let mut num_indices = std::collections::HashMap::new();
-    for (i, &num) in nums.iter().enumerate() {
-        let complement = target - num;
-        if let Some(&j) = num_indices.get(&complement) {
-            return (j, i);
+    for i in 0..nums.len() {
+        for j in (i + 1)..nums.len() {
+            if nums[i] + nums[j] == target {
+                return (i, j);
+            }
         }
-        num_indices.insert(num, i);
     }
-    panic!("No two sum solution found");
+    panic!("No two sum solution");
 }
 
 // testing
