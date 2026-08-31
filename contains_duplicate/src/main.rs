2e63870f47be639bf1772a5534f35ef96c55dfb1
Aiden <aiden@rivi.us>
2026-08-31 10:29:53 -0400

    a

--- contains_duplicate/src/main.rs
+++ contains_duplicate/src/main.rs
@@ -1,4 +1,17 @@
 // if the array contains any value that appears twice, return true, else return false
-fn contains_duplicate(nums) {
-    
+fn contains_duplicate(nums: Vec<i32>) -> bool {
+    for i in 0..nums.len() {
+        for j in i + 1..nums.len() {
+            if nums[i] == nums[j] {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+//tests
+fn main() {
+    let nums = vec![1, 2, 3, 4, 5];
+    println!("{}", contains_duplicate(nums));
 }
