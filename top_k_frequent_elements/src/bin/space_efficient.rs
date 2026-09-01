6f0a09b4a6569d212265563d11ae43f65f075154
Aiden <aiden@rivi.us>
2026-09-01 15:39:10 -0400

    a

--- top_k_frequent_elements/src/bin/space_efficient.rs
+++ top_k_frequent_elements/src/bin/space_efficient.rs
@@ -4,7 +4,7 @@ fn frequent_elements(nums: Vec<i32>, k: usize) -> Vec<i32> {
     let mut counts = HashMap::new();
     for num in nums {
         *counts.entry(num).or_insert(0) += 1;
-        
+        // assume that the vector you are given is sorted. add if statements after line 6 to remove the unnessecary values in the hashmap that arent going to be in the end for sure because k or more other values have more than that value
     }
     let mut elements: Vec<(i32, i32)> = counts.into_iter().collect();
     elements.sort_by(|a, b| b.1.cmp(&a.1));
