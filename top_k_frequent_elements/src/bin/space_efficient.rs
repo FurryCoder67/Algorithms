01d279739bf191a75bc87c3bfdb4509aa6cff172
Aiden <aiden@rivi.us>
2026-09-01 15:49:52 -0400

    a

--- top_k_frequent_elements/src/bin/space_efficient.rs
+++ top_k_frequent_elements/src/bin/space_efficient.rs
@@ -4,6 +4,12 @@ fn frequent_elements(nums: Vec<i32>, k: usize) -> Vec<i32> {
     let mut counts = HashMap::new();
     for num in nums {
         *counts.entry(num).or_insert(0) += 1;
+        if counts.len() > k + 1 {
+            let min_key = map.iter().min_by_key(|(_, c)| **v).map(|(_, c)| *k)
+            if let Some(key) = min_key {
+                map.remove(&key);
+            }
+        }
     }
     let mut elements: Vec<(i32, i32)> = counts.into_iter().collect();
     elements.sort_by(|a, b| b.1.cmp(&a.1));
