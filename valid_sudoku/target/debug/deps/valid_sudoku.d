be229694f325e08d57e75b906d3a40300f086a21
Aiden <aiden@rivi.us>
2026-09-03 13:23:51 -0400

    a

---
+++ valid_sudoku/target/debug/deps/valid_sudoku.d
@@ -0,0 +1,5 @@
+C:\Users\aiden\Documents\Code\Algorithms\valid_sudoku\target\debug\deps\valid_sudoku.d: src\main.rs
+
+C:\Users\aiden\Documents\Code\Algorithms\valid_sudoku\target\debug\deps\valid_sudoku.exe: src\main.rs
+
+src\main.rs:
---
+++ valid_sudoku/target/debug/valid_sudoku.d
@@ -0,0 +1,1 @@
+C:\Users\aiden\Documents\Code\Algorithms\valid_sudoku\target\debug\valid_sudoku.exe: C:\Users\aiden\Documents\Code\Algorithms\valid_sudoku\src\main.rs
