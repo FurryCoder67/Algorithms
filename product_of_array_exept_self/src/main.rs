749c00a3987e7ca114299649991066e1e4bcf0b3
Aiden <aiden@rivi.us>
2026-09-03 11:08:24 -0400

    a

--- product_of_array_exept_self/src/main.rs
+++ product_of_array_exept_self/src/main.rs
@@ -1,6 +1,7 @@
 // Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i]
 // The product of any prefix or suffix of nums is guaranteed to fit in a 32 bit integer
 // You must write an algorithm that runs in O(n) time and without using the division operation
+// Do this in rust with : fn array_product(nums: Vec(i32)) -> Vec(i32) {} and a fn main for the tests
 fn array_product(nums: Vec(i32)) -> Vec(i32) {
-    
+
 }
