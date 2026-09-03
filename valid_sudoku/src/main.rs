208b6d17311bf6e49b68bbe18e9121ce066fa106
Aiden <aiden@rivi.us>
2026-09-03 13:03:14 -0400

    a

---
+++ valid_sudoku/src/main.rs
@@ -0,0 +1,6 @@
+// Determine if a 9x9 Sudoku board is valid.
+// Only if the filled cells need to be validated according to the following rules:
+// 1. Each row must contain the digits 1-9 without repitition
+// 2. Each column must contain the digits 1-9 without repitition
+// 3. Each of the nine  3x3 sub-boxes of the grid must contain the digits 1-9 without repitition.
+// You are to do this in Rust with a fn valid_sudoku that takes some nested vectors and a fn main for the testing
