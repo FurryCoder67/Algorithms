470192229e6b3d6df6c494e8f20dba7d6e8920a8
Aiden <aiden@rivi.us>
2026-09-03 13:04:32 -0400

    a

--- valid_sudoku/src/main.rs
+++ valid_sudoku/src/main.rs
@@ -1,8 +1,3 @@
-// Determine if a 9x9 Sudoku board is valid.
-// Only if the filled cells need to be validated according to the following rules:
-// 1. Each row must contain the digits 1-9 without repitition
-// 2. Each column must contain the digits 1-9 without repitition
-// 3. Each of the nine  3x3 sub-boxes of the grid must contain the digits 1-9 without repitition.
 use std::collections::HashSet;
 
 fn valid_sudoku(board: Vec<Vec<char>>) -> bool {
